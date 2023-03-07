503dcb5334cfd56dfa16f82115002cc0e3e7cf99
dotjrb <dev.jcrb@gmail.com>
2023-03-07 00:53:14 -0500

    Se añade el script para el funcionamiento del reloj

--- code/watchCode.js
+++ code/watchCode.js
@@ -1,9 +1,56 @@
-let reloj = document.getElementById("reloj");
+(function(){
 
-function llamaAlReloj() {
-    let time = new Date()
-    reloj.innerHTML = time
-    console.log(time)
-}
+    var actualizarHora = function() {
+        var fecha = new Date(),
+            horas = fecha.getHours(),
+            ampm,
+            minutos = fecha.getMinutes(),
+            segundos = fecha.getSeconds(),
+            diaSemana = fecha.getDay(),
+            dia = fecha.getDate(),
+            mes = fecha.getMonth(),
+            year = fecha.getFullYear();
 
-llamaAlReloj()
+        var pHoras = document.getElementById("hour"),
+            pAMPM = document.getElementById("ampm"),
+            pMinutos = document.getElementById("minutes"),
+            pSegundos = document.getElementById("seconds"),
+            pDiaSemana = document.getElementById("dayWeek"),
+            pDia = document.getElementById("dayDate"),
+            pMes = document.getElementById("month"),
+            pYear = document.getElementById("year");
+
+        var semana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+        pDiaSemana.textContent = semana[diaSemana];
+        pDia.textContent = dia;
+        var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+        pMes.textContent = meses[mes];
+        pYear.textContent = year;
+
+        if (horas >= 12) {
+          horas = horas - 12;
+          ampm = "PM";
+        } else {
+            ampm = "AM";
+        };
+        if (horas === 0) {
+            horas = 12;
+        };
+        if (horas < 10) {
+            horas = "0" + horas;
+        };
+        if (minutos < 10) {
+            minutos = "0" + minutos;
+        }
+        if (segundos < 10) {
+            segundos = "0" + segundos;
+        };
+        pHoras.textContent = horas;
+        pMinutos.textContent = minutos;
+        pSegundos.textContent = segundos;
+    };
+
+    actualizarHora();
+
+    var intervalo = setInterval(actualizarHora, 1000);
+}())
